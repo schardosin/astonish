9683e3fc81426c04d5d4c9b5cd3e070d7ac1c4ac
schardosin <schardosin@users.noreply.github.com>
2025-05-02 03:44:15 +0000

    deploy: ea6ef4c4d0edc1f3becfa4c2710949b49565fed8

---
+++ assets/js/acecf23e.4eea9598.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/astonish/blog","blogTitle":"Blog","authorsListPath":"/astonish/blog/authors"}')}}]);
